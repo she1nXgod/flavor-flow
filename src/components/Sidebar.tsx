351ca736a974b8e84370c35701a39e959fc65a4c
she1nXgod <domenator156@mail.ru>
2026-05-21 22:53:00 +0300

    add dashboard sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,34 @@
+import { Nav } from 'react-bootstrap';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Nav variant='pills' className='flex-column gap-3'>
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        <img src='/all-recipes.svg' alt='All recipes' className='icon mx-2' />
+        All Recipes
+      </Nav.Item>
+
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        ☀ Breakfast
+      </Nav.Item>
+
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        🍔 Lunch
+      </Nav.Item>
+
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        🍗 Dinner
+      </Nav.Item>
+
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        🍕 Pizza
+      </Nav.Item>
+
+      <Nav.Item className='d-flex justify-content-center align-items-center text-center glass-card sidebar-card fw-bold'>
+        🍰 Desserts
+      </Nav.Item>
+    </Nav>
+  );
 };
 
 export default Sidebar;
